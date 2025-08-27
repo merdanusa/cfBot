760994b58209ae050e9b76e93c43759ed80deddb
Merdan <wisew.tube@gmail.com>
2025-08-28 02:51:59 +0500

    init commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { Telegraf } = require("telegraf");
 const botHandler = require("./botHandler");
+const connectDB = require("./config/database");
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+connectDB();
+
 botHandler(bot);
 
 bot.launch();
-console.log("👨‍💻 sbp31SpyBot is running... 🤖");
+console.log("🤖 SBP31 Spy Bot is running...");
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
